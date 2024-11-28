2e78efbb74925d068f8c5c6bca34ea4eb92b4745
Firoz fahim <52341506+F1roz@users.noreply.github.com>
2024-11-28 10:40:41 +0600

    Add files via upload

--- football_data.py
+++ football_data.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
 import pandas as pd
 import time
 
@@ -21,12 +22,24 @@ driver.get(website)
 all_matches_button = driver.find_element(By.XPATH, '//label[@analytics-event="All matches"]')
 all_matches_button.click()
 
-matches = driver.find_elements(By.TAG_NAME, 'tr')
+dropdown = Select(driver.find_element(By.ID, 'country'))
+#dropdown.select_by_visible_text('Spain')
+country_options= [option.text for option in dropdown.options]
+time.sleep(10)
+
+
 
 date =[]
 home_team=[]
 score= []
 away_team= []
+country_list =[]
+
+for country in country_options:
+    dropdown.select_by_visible_text(country)
+    time.sleep(15)
+
+matches = driver.find_elements(By.TAG_NAME, 'tr')
 
 for match in matches:
     date.append(match.find_element(By.XPATH, '//tr/td[1]').text)
@@ -35,8 +48,10 @@ for match in matches:
     #print(home)
     score.append(match.find_element(By.XPATH, '//tr/td[3]').text)
     away_team.append(match.find_element(By.XPATH, '//tr/td[4]').text)
+    country_list.append(country)
+
 driver.quit()
-df= pd.DataFrame({'date': date, 'home_team': home_team, 'score': score, 'away_team': away_team})
+df= pd.DataFrame({'date': date, 'country':country_list, 'home_team': home_team, 'score': score, 'away_team': away_team})
 df.to_csv('football_data.csv', index = False)
 print(df)
 
